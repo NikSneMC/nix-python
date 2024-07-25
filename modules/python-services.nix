a6d8de260c3b30de45251a805b1f6ec3ed3cdee1
NikSne <commits@niksne.ru>
2024-07-25 14:25:28 +0300

    fix: python-services.nix

--- modules/python-services.nix
+++ modules/python-services.nix
@@ -287,7 +287,7 @@ in
               reload = ''
                 ${postStop}
                 ${preStart}
-              '' + conf.extraReload;
+              '';
 
               preStart =
                 let
